4c7b2e0b6aff7447d998fe9d5ab474cd283138c2
Maksim Budzko <MBudko@alfabank.ru>
2021-04-05 02:01:34 +0300

    remove it. use function types with receiver (variable context)

--- app/src/main/java/by/nerallan/dsl_template/ArticleDSL.kt
+++ app/src/main/java/by/nerallan/dsl_template/ArticleDSL.kt
@@ -1,15 +1,15 @@
 package by.nerallan.dsl_template
 
-fun page(block: (Page) -> Unit): Page = Page().apply(block)
-fun pageBlock(block: (PageBlock) -> Unit): PageBlock = PageBlock().apply(block)
+fun page(block: Page.() -> Unit): Page = Page().apply(block)
+fun pageBlock(block: PageBlock.() -> Unit): PageBlock = PageBlock().apply(block)
 
 class ArticleDSL {
 
     fun generatePage() {
-        val page = page { page ->
-            page.number = 1
-            page.componentPayload = pageBlock {
-                it.content = "this is article content"
+        val page = page {
+            number = 1
+            componentPayload = pageBlock {
+                content = "this is article content"
             }
         }
     }
