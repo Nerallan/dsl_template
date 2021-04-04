cc78fb763211055487e75e87567bcacded12ae20
Maksim Budzko <MBudko@alfabank.ru>
2021-04-05 01:54:52 +0300

    use apply scope fun

--- app/src/main/java/by/nerallan/dsl_template/ArticleDSL.kt
+++ app/src/main/java/by/nerallan/dsl_template/ArticleDSL.kt
@@ -1,22 +1,13 @@
 package by.nerallan.dsl_template
 
-fun page(block: (Page) -> Unit): Page {
-    val page = Page()
-    block(page)
-    return page
-}
+fun page(block: (Page) -> Unit): Page = Page().apply(block)
 
 class ArticleDSL {
 
     fun generatePage() {
-        // imperative style. with consistent spelling
-        val pageBlock = PageBlock("this is article content")
-        var page = Page(number = 1, componentPayload = pageBlock)
-
-        // declarative style
-        var page2 = page {
+        val page = page {
             it.number = 1
-            it.componentPayload = pageBlock
+            it.componentPayload = PageBlock("this is article content")
         }
     }
 }
